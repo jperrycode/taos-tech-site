cde11df3b2409bccf2943d1ca580935e4396367d
taoshaus <jadamsperry@gmail.com>
2023-09-23 07:09:58 -0600

    updates

--- main/settings_dev.py
+++ main/settings_dev.py
@@ -45,7 +45,7 @@ DEBUG = env("DEBUG", False) == True
 
 DJANGO_ALLOWED_HOSTS = env("DJANGO_ALLOWED_HOSTS")
 
-ALLOWED_HOSTS = env("ALLOWED_HOSTS").split(',')
+ALLOWED_HOSTS = ['127.0.0.1:8000','localhost']
 
 DEVELOPMENT_MODE = env("DEVELOPMENT_MODE") 
 
@@ -179,7 +179,7 @@ USE_L10N = False
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+STATIC_ROOT = os.path.join(BASE_DIR, "/staticfiles/")
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
