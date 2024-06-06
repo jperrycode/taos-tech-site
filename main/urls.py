5ddb35bb936dfaa09e94b16a61b90801fd48fb29
taoshaus <jadamsperry@gmail.com>
2024-06-06 08:48:45 -0600

    redirect

--- main/urls.py
+++ main/urls.py
@@ -2,11 +2,13 @@ from django.contrib import admin
 from django.urls import include, path
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.generic.base import RedirectView
 
 admin.site.site_header = 'Taos Haus Tech'
 admin.site.index_title = 'THTS Data'
 
 urlpatterns = [
+    path('', RedirectView.as_view(permanent=True, url='tech-solutions/')),
     path('admin/', admin.site.urls),
     path('tech-solutions/', include('frontend.urls')),
     path('dj/', include('djapp.urls')),
